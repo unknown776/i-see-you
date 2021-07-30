d6957727a3339ba3bd320b3cf2902d6cf9c52823
unknown776 <79632873+unknown776@users.noreply.github.com>
2021-07-30 16:26:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector("body").addEventListener('mousemove', eyeball);
-
+let music = new Audio("i_see_you.mp3");
+alert("Please click anywhere in this window!")
 function eyeball() {
     let eye = document.querySelectorAll(".eye");
     eye.forEach(function(eye) {
@@ -9,4 +10,8 @@ function eyeball() {
         let rot = (radian * (180 / Math.PI) * -1) + 270;
         eye.style.transform = `rotate(${rot}deg)`;
     });
-}
+};
+
+setTimeout(() => {
+    music.play()
+}, 10000);
